e7a3e904ec8fa192f65c65a77ca67fd3ef93fa48
LeUwUr <evadevizcarra@gmail.com>
2021-06-03 13:15:01 -0700

    Operadores y variables - video 4

---
+++ video4.py
@@ -0,0 +1,23 @@
+nombre = 5
+type (nombre) # <class 'init>
+
+nombre1 = "Juan"
+type (nombre1) # <class 'str'>
+
+nombre2 = 5.2
+type(nombre2) # <class 'float'>
+
+mensaje = """Esto es un mensaje
+    con tres saltos
+    de linea """
+print(mensaje)
+
+numero3 = 5
+numero4 = 7
+if numero3>numero4:
+    print("el numero 3 es mayor")
+else:
+    print("el numero 4 es mayor") # = es 'asignacion', == es 'comparacion'
+
+
+
