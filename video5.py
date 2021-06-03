8455da1868bd7eeedc80729a584d0aa808007d7b
LeUwUr <evadevizcarra@gmail.com>
2021-06-03 15:14:12 -0700

    funciones - video 5

---
+++ video5.py
@@ -0,0 +1,10 @@
+def mensaje(): #declaracion
+
+    print("estamos aprendiendo Python")
+    print("estamos aprendiendo instrucciones basicas") #cuerpo de la funcion
+    print("poco a poco iremos avanzando")
+
+mensaje() #llamada a la funcion
+
+print("Ejecutando codigo de funcion")
+mensaje()
