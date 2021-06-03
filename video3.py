be38f079c78611a08128210862f236b3ff9b9e78
LeUwUr <evadevizcarra@gmail.com>
2021-06-03 12:54:58 -0700

    funciones de print en video 3

---
+++ video3.py
@@ -0,0 +1,18 @@
+print ("Hola a todos")
+
+print ("Hola mundo"); print("Adios mundo cruel")
+
+#Usa gato para comentar
+
+mi_nombre="Mi nombre es Eva"
+print (mi_nombre)
+
+mi_nombre2="mi nombre es\
+Eva"
+print (mi_nombre2)
+
+a=0
+for i in range(5):
+    a+=1                #Esto pertenece a la funcion "For"
+    print(a)
+
