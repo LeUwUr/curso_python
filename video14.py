61b839d8c7977736bfdf5f329921bc7c059f280a
LeUwUr <evadevizcarra@gmail.com>
2021-07-08 00:13:33 -0700

    Bucles 4 For (Determindos) - video 14

---
+++ video14.py
@@ -0,0 +1,5 @@
+for i in ["primavera","verano","otonio","inviero"]: 
+    print("hola")
+
+#no importa lo que haya dentro de la lista, la funcion imprimira la palabra indicada las veces
+# de palabras en una tupla
