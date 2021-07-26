fe6469abe273f35874dae0c0fde8044f860286ea
LeUwUr <evadevizcarra@gmail.com>
2021-07-26 16:35:11 -0700

    POO 8 (Herencia 3) - video 31

---
+++ video31.py
@@ -0,0 +1,70 @@
+class vehiculos():
+
+    def __init__(self, marca, modelo):
+
+        self.marca=marca
+        self.modelo=modelo
+        self.enmarcha=False
+        self.acelera=False
+        self.frena=False
+
+    def arrancar(self):
+        self.enmarcha=True
+
+    def acelerar(self):
+        self.acelera=True
+
+    def frenar(self):
+        self.frena=True
+
+    def estado(self):
+        print("MARCA: ", self.marca, "\nMODELO: ", self.modelo, "\nEN MARCHA: ",
+            self.enmarcha, "\nACELERANDO: ", self.acelera, "\nFRENADO: ", self.frena)
+
+class furgoneta(vehiculos):
+
+    def carga(self, cargar):
+        self.cargado=cargar
+
+        if(self.cargado):
+            return "La furgoneta esta cargada"
+
+        else:
+            return "La furgoneta no esta cargada"
+
+class VElectricos(vehiculos):
+    def __init__(self, marca, modelo):
+
+        super().__init__(marca, modelo) #AQUI EMPIEZA VIDEO 31
+
+        self.autonomia=100
+
+    def cargarEnergia(self):
+        self.cargando=True
+        
+        
+
+class moto(vehiculos):
+    hcaballito=""
+    def caballito(self):
+        self.hcaballito="Voy haciendo el caballito"
+
+    def estado(self):
+        print("MARCA: ", self.marca, "\nMODELO: ", self.modelo, "\nEN MARCHA: ",
+            self.enmarcha, "\nACELERANDO: ", self.acelera, "\nFRENADO: ", self.frena, "\n", self.hcaballito)
+
+miMoto=moto("Honda", "CBR")
+miMoto.caballito()
+miMoto.estado()
+
+print("*+*+*+*+ SIGUIENTE VEHICULO *+*+*+*+")
+
+miFurgoneta=furgoneta("Renault", "KAGOO")
+miFurgoneta.arrancar()
+miFurgoneta.estado()
+print(miFurgoneta.carga(True))
+
+class bicicletaElectrica(VElectricos,vehiculos):
+    pass
+    
+miBici=bicicletaElectrica("Orbea", "273")
