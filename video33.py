6d13c69821e00dff1705ebeea44f6482143684b6
LeUwUr <evadevizcarra@gmail.com>
2021-07-27 00:06:50 -0700

    Metodos de cadenas - Video 33

---
+++ video33.py
@@ -0,0 +1,7 @@
+usuario="eva"
+
+print("El nombre es:", usuario.upper()) # CONVIERTE EL TEXTO A MAYUSCULAS
+
+print("El nombre es:", usuario.lower()) # CONVIERTE EL TEXTO A MINUSCULAS
+
+print("El nombre es:", usuario.capitalize()) # CONVIERTE LA PRIMERA LETRA A MAYUSCULA
