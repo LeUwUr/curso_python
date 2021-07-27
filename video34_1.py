5ee13890d4744600d4d53a2964d6378348dd494f
LeUwUr <evadevizcarra@gmail.com>
2021-07-27 13:23:47 -0700

    Modulos (parte 2) - Video 34

---
+++ video34_1.py
@@ -0,0 +1,10 @@
+#import video34
+from video34 import * # EL ASTERISCO REPRESENTA TODAS LA FUNCIONES 
+
+sumar(5,7)
+restar(5,7)
+multiplicar(5,7)
+
+#video34.sumar(7,5)
+#video34.restar(7,5)
+#video34.multiplicar(7,5)
