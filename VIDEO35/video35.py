035d058e5528bae4acb1b5767b8131adc4be95a6
LeUwUr <evadevizcarra@gmail.com>
2021-07-29 15:37:28 -0700

    Paquetes - Video 35

---
+++ VIDEO35/video35.py
@@ -0,0 +1,17 @@
+def sumar(op1,op2):
+    print("El resultado de la SUMA es:", op1+op2)
+
+def restar(op1,op2):
+    print("El resultado de la RESTA es:", op1-op2)
+
+def multiplicar(op1,op2):
+    print("El resultado de la MULTIPLICACION es:", op1*op2)
+
+def dividir(dividendo,divisor):
+    print("El resultado de la DIVISION es:", dividendo/divisor)
+
+def potencia(base,exponente):
+    print("El resultado de la POTENCIA es:", base**exponente)
+
+def redondear(numero):
+    print("El resultado del REDONDEO es:", round(numero))
